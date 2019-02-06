c9a775f3539524fbb7eb03d840e940f74aae32c0
bhavesh1974 <bhavesh1974@gmail.com>
2019-02-07 01:18:07 +0530

    Iniital Commit

--- RestApi.sql
+++ RestApi.sql
@@ -1,10 +1,22 @@
-CREATE TABLE IF NOT EXISTS `token` (
+CREATE TABLE IF NOT EXISTS `sales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `salesDate` date NOT NULL,
+  `customer` varchar(50) NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `qty` decimal(10,3) NOT NULL DEFAULT '0.000',
+  `rate` decimal(10,3) NOT NULL DEFAULT '0.000',
+  `taxPercent` decimal(10,3) NOT NULL DEFAULT '0.000',
+  `remark` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `tokens` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `userId` varchar(50) NOT NULL,
   `token` varchar(500) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` varchar(50) NOT NULL,
@@ -19,6 +31,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `verificationToken` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) 
-
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
